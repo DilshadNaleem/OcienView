8b54864ff46d2687ce0b9f403552ce1235583bfc
DilshadNaleem <dilshadnaleem13@gmail.com>
2026-01-28 17:35:29 +0530

    Implementing necessary configurations including java files

---
+++ ocean-view-frontend/src/components/Customer Tasks/EditProfileCustomer.jsx
@@ -0,0 +1,137 @@
+import pImage from "../../assets/image/p.jpg";
+import { useState, useRef } from "react";
+import { FaCamera } from "react-icons/fa";
+
+function CustomerEditProfile() 
+{
+    const [profileImage, setProfileImage] = useState(pImage);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const fileInputRef = useRef(null);
+
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if(file) {
+            setSelectedImage(file);
+            const imageUrl = URL.createObjectURL(file);
+            setProfileImage(imageUrl);
+        }
+    };
+
+    const handleImageClick = () => {
+        fileInputRef.current.click();
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const formObject = Object.fromEntries(formData.entries());
+
+
+        if(selectedImage) {
+            formObject.profileImage = selectedImage;
+        }
+
+        console.log("Form Data Submitted: ", formObject);
+        console.log("Profile Image File: ", selectedImage);
+
+         if (selectedImage) {
+        URL.revokeObjectURL(profileImage);
+    }
+    };
+
+    return(
+    <div className="main-register">  
+        <div className="register">     
+            <div className="heading">
+                <h3>Customer Edit Profile</h3>
+            </div>
+
+            <div className="form-container">
+                <div className="form-body">
+                    <form action="" className='register-form' onSubmit={handleSubmit}>
+
+                        <div className="input-group-img">
+                            <div className="profile-image-container">
+                                <img src={profileImage} alt="Profile"
+                                className="profile-image" 
+                                />
+
+                                 <div 
+                                        className="image-overlay"
+                                        onClick={handleImageClick}
+                                    >
+                                        <FaCamera className="camera-icon" />
+                                        <span className="change-photo">Change Photo</span>
+                                    </div>
+                                     <input
+                                    type="file"
+                                    ref={fileInputRef}
+                                    onChange={handleImageChange}
+                                    accept="image/*"
+                                    style={{ display: 'none' }}
+                                />
+
+                            </div>
+                        </div>
+                        <div className="input-group">
+                            <input 
+                                type="text" 
+                                name="firstName"
+                                placeholder='Enter First Name' 
+                                className="input"
+                            />
+                        </div>
+
+                         <div className="input-group">
+                            <input 
+                                type="text" 
+                                name="lastName"
+                                placeholder='Enter Last Name' 
+                                className="input"
+                            />
+                        </div>
+
+                        <div className="input-group">
+                            <input 
+                                type="text" 
+                                name="email"
+                                placeholder='Enter Email' 
+                                className="input"
+                                readOnly
+                            />
+                        </div>
+
+                        <div className="input-group">
+                            <input 
+                                type="text" 
+                                name="nic"
+                                placeholder='Enter NIC' 
+                                readOnly
+                                className="input"
+                            />
+                        </div>
+
+                        <div className="input-group">
+                            <input 
+                                type="text" 
+                                name="contactNumber"
+                                placeholder='Enter Contact Number' 
+                                className="input"
+                                readOnly
+                            />
+                        </div>
+                        
+                         <button className='click-btn'>Edit Profile</button>
+                    </form>
+                        
+                </div>
+            </div>
+        </div> 
+    </div>
+        
+    )
+}
+
+export default CustomerEditProfile;
